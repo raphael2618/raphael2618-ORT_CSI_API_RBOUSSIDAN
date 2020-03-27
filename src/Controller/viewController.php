9aa56c3dda30aca5e5d766afb107f6010dcb524b
raphael boussidan <boussidan.raphael@gmail.com>
2020-03-27 10:09:28 +0100

    feat(View) add controller)

---
+++ src/Controller/viewController.php
@@ -0,0 +1,24 @@
+<?php
+namespace App\Controller;
+use Symfony\Component\HttpClient\CurlHttpClient;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+class viewController extends AbstractController {
+    /**
+     *  @Route("/view", name="proj_view") 
+     * */
+     public function view() {
+        $clientCurl = new CurlHttpClient();
+        
+        $var=$clientCurl->request("POST", "http://concoursphoto.ort-france.fr/api/matrice",
+        ['headers' => ['Content-Type' => 'application/json'],'body' => '{}']);
+
+        $Content=$var->ToArray();
+
+        dd($Content);
+         return new Response('RAPHAEL ORT');
+
+     }
+
+}
